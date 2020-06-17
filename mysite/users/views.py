b9f3a612e85d68e357db87126cae82476e5b216a
Ravi Shukla <rshuklabbb@gmail.com>
2020-06-17 22:25:44 +0530

    Added profile image of authore beside there blog post on Homepage, Added update functionality for user profile at profile page

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfielUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -19,4 +19,20 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfielUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save() 
+            messages.success(request, f'Your Account has been Updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfielUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
