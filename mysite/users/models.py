814d1c88461aa51158e69c8015f5efbfb5bc52dd
Ravi Shukla <rshuklabbb@gmail.com>
2020-06-18 21:31:36 +0530

    Added CRUD functionality for blogs

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -9,8 +9,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile ,self).save(*args, **kwargs)
         max_size = 300
         img = Image.open(self.image.path)
 
