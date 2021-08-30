982b668dd003a6840efc9d27c9793625234a7696
Ravi Shukla <rshuklabbb@gmail.com>
2021-08-30 11:48:26 +0530

    Updated file system and gitignore files

---
+++ blog/blog/users/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+        max_size = 300
+        img = Image.open(self.image.path)
+
+        if img.height > max_size or img.width > max_size:
+            output_size = (max_size, max_size)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
